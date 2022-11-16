dcb4b6f0b3976d76281029489583225b7d8cb806
Alinevictoriasouza <alynevictoriasouza47@gmail.com>
2022-11-16 14:53:23 -0400

    Adicionado código de criptografia

---
+++ seeders/20220727021551-adicionar-usarios.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('usuarios', [
+      { nome: 'John Doe', senha: '123', usuario: "doe"},
+      { nome: 'Picolo', senha: '123', usuario: "luiz" },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('usuarios', null, {});
+  }
+};
